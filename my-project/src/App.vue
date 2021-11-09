698dd3cfb82076ac7800c5a431b7f6cd3ef274d2
thanyapisit Buapakong <nyman3527@gmail.com>
2021-11-09 20:39:28 +0700

    add vue module to limit message

---
+++ my-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div class="container">
+          <h2 align = "center ">limit message</h2>
+    <hr>
+    <div class= "input-group">
+      <div class= "input-group-prepend">
+      </div>
+      <input type= "text" class= "form-control" >
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+}
+</script>
