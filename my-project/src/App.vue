3f25cdf0c323e364db421900d4e85d2fe13a0882
thanyapisit Buapakong <nyman3527@gmail.com>
2021-11-10 11:21:31 +0700

    add limit 20 message

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -5,17 +5,26 @@
     <hr>
     <div class= "input-group">
       <div class= "input-group-prepend">
+        <span class="input-group-text" v-text="max-message.length">20</span>
       </div>
-      <input type= "text" class= "form-control" >
+      <input type= "text" class= "form-control"  v-model="message" :maxlength="max">
     </div>
   </div>
   </div>
 </template>
-
 <script>
+
 
 export default {
   name: 'App',
-
+  data() {
+    return {
+      message: '',
+      max : 20
+    }
+  }
 }
+
+
+
 </script>
